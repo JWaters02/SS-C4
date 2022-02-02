1e5a70bf6248bac8ed21247982cc9334bf372738
JWaters02 <watersjoshua2002@gmail.com>
2022-02-02 14:49:19 +0000

    Labs inside this project (will be deleted later)

---
+++ Shell/src/main/java/ss/shell/labs/Operator.java
@@ -0,0 +1,15 @@
+package ss.shell.labs;
+
+public class Operator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+
+    public int multiplication(int a, int b){
+        return a*b;
+    }
+
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
